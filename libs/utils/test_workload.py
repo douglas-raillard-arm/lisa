97c520bb9a0da0bd8dcc9ba42fbc71fe5dc5c878
Valentin Schneider <valentin.schneider@arm.com>
2018-08-21 18:16:37 +0100

    WIP: test_workload: all generic.py is working now!

--- libs/utils/test_workload.py
+++ libs/utils/test_workload.py
@@ -1,9 +1,15 @@
 import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from math import isnan
+
+# TODO: killme.jpeg
 from executor import Executor
 
-from serialize import YAMLSerializable
-
 from perf_analysis import PerfAnalysis
+from serialize import YAMLSerializable
 from wlgen.rta import RTA
 
 # from wa import Metric
@@ -71,7 +77,11 @@ class Metric(object):
 
 class ResultBundle(object):
     """
-    Bundle for storing test results such as metrics or pass/fail
+    Bundle for storing test results
+
+    :param passed: Indicates whether the associated test passed.
+        It will also be used as the truth-value of a ResultBundle.
+    :type passed: boolean
     """
     def __init__(self, passed):
         self.passed = passed
@@ -81,19 +91,47 @@ class ResultBundle(object):
         return self.passed
 
     def add_metric(self, metric):
+        """
+        Lets you append several test :class:`Metric` to the bundle.
+
+        :param metric: Metric to add to the bundle
+        :type metric: Metric
+        """
         self.metrics.append(metric)
 
 import shutil
 
 class TestBundle(YAMLSerializable):
     """
-    Description of a Lisa test bundle
+    A LISA test bundle.
 
-    :meth:`from_target` will use :meth:`from_disk` to create the actual
-    instance, which guarantees that there is no discrepancy between the target
-    and disk methods. If it makes no sense for the use-case to save any kind of
-    state on the disk (e.g. the end result of the target execution is just a
-    boolean value), that can be overriden.
+    :param res_dir: Directory from where the target execution artifacts reside.
+        This will also be used to dump any artifact generated in the test code.
+    :type res_dir: str
+
+    :Design notes:
+        * :meth:`from_target` will collect whatever artifacts are required
+          from a given target, and will then create a :class:`TestBundle`.
+        * :meth:`from_path` will use whatever artifacts are available in a
+          given directory, and will then create a :class:`TestBundle`.
+
+       .. warning:: Those artifacts must be created by :meth:`from_target` to allow
+          "offline" replaying, that is, we must be able to re-run tests without
+          having access to a target.
+
+
+    Thus, creating a Bundle from a live target would be done as follows:
+
+    >>> bundle = TestBundle.from_target(test_env, "my/res/dir")
+
+    And you could run some test on the collected data like so:
+
+    >>> res_bundle = bundle.test_foo()
+
+    Offline replaying would be done like so:
+
+    >>> bundle = TestBundle.from_path("/my/res/dir")
+    >>> res_bundle = bundle.test_foo()
     """
 
     def __init__(self, res_dir):
@@ -103,8 +141,6 @@ class TestBundle(YAMLSerializable):
     def _from_target(cls, te, res_dir):
         """
         Internals of the target factory method.
-
-        TODO: Please give me a proper name
         """
         raise NotImplementedError()
 
@@ -156,15 +192,41 @@ from trace import Trace
 from energy_model import EnergyModel
 class GenericTestBundle(TestBundle):
     """
-    Yadda yadda, EAS placement and energy test cases
+    "Abstract" class for generic synthetic tests.
+
+    :param res_dir: See :class:`TestBundle`
+    :type res_dir: str
+
+    :param nrg_model: The energy model of the platform the synthetic workload
+      was run on
+    :type nrg_model: EnergyModel
+
+    :param rtapp_params: The rtapp parameters used to create the synthetic
+      workload. That happens to be what is returned by :meth:`create_rtapp_params`
+    :type rtapp_params: dict
+
+    This class provides :meth:`test_slack` and :meth:`test_task_placement` to
+    validate the basic behaviour of EAS.
     """
 
     ftrace_conf = {
         "events" : ["sched_switch"],
     }
+    """
+    The FTrace configuration used to record a trace while the synthetic workload
+    is being run.
+    """
 
     @property
     def trace(self):
+        """
+
+        :returns: a Trace
+
+        Having the trace as a property lets us defer the loading of the actual
+        trace to when it is first used. Also, this prevents it from being
+        serialized when calling :meth:`to_path`
+        """
         if not self._trace:
             self._trace = Trace(self.res_dir, events=self.ftrace_conf["events"])
 
@@ -182,7 +244,9 @@ class GenericTestBundle(TestBundle):
     @classmethod
     def create_rtapp_params(cls, te):
         """
-        :returns: dict
+        :returns: a :class:`dict` with task names as keys and :class:`RTATask` as values
+
+        This is the method you want to override
         """
         raise NotImplementedError()
 
@@ -203,6 +267,14 @@ class GenericTestBundle(TestBundle):
 
         return cls(res_dir, te.nrg_model, rtapp_params)
 
+    @classmethod
+    def min_cpu_capacity(cls, te):
+        return min(te.target.sched.get_capacities().values())
+
+    @classmethod
+    def max_cpu_capacity(cls, te):
+        return max(te.target.sched.get_capacities().values())
+
     def test_slack(self, negative_slack_allowed_pct=15):
         """
         Assert that the RTApp workload was given enough performance
@@ -214,10 +286,10 @@ class GenericTestBundle(TestBundle):
             activations with negative slack.
         :type negative_slack_allowed_pct: int
 
-        Use :class:PerfAnalysis to find instances where the RT-App workload
+        Use :class:`PerfAnalysis` to find instances where the RT-App workload
         wasn't able to complete its activations (i.e. its reported "slack"
         was negative). Assert that this happened less than
-        `negative_slack_allowed_pct` percent of the time.
+        :attr:`negative_slack_allowed_pct` percent of the time.
         """
         pa = PerfAnalysis(self.res_dir)
 
@@ -242,7 +314,23 @@ class GenericTestBundle(TestBundle):
 
         return res
 
-    def get_task_utils_df(self):
+    def _get_start_time(self):
+        """
+        Get the time where the first task spawned
+        """
+        tasks = self.rtapp_params.keys()
+        sdf = self.trace.data_frame.trace_event('sched_switch')
+        start_time = self.trace.start_time + self.trace.time_range
+
+        for task in tasks:
+            pid = self.trace.getTaskByName(task)
+            assert len(pid) == 1, "getTaskByName returned more than one PID"
+            pid = pid[0]
+            start_time = min(start_time, sdf[sdf.next_pid == pid].index[0])
+
+        return start_time
+
+    def _get_expected_task_utils_df(self):
         """
         Get a DataFrame with the *expected* utilization of each task over time
 
@@ -260,11 +348,12 @@ class GenericTestBundle(TestBundle):
 
         # First we'll build a dict D {time: {task_name: util}} where D[t][n] is
         # the expected utilization of task n from time t.
-        for task, params in self.rtapp_params["tasks"].iteritems():
-            time = self.get_start_time(experiment) + params.get('delay', 0)
+        for task, params in self.rtapp_params.iteritems():
+            # time = self.get_start_time(experiment) + params.get('delay', 0)
+            time = params.delay_s
             add_transition(time, task, 0)
-            for _ in range(params.get('loops', 1)):
-                for phase in params['phases']:
+            for _ in range(params.loops):
+                for phase in params.phases:
                     util = (phase.duty_cycle_pct * util_scale / 100.)
                     add_transition(time, task, util)
                     time += phase.duration_s
@@ -274,7 +363,39 @@ class GenericTestBundle(TestBundle):
         df = pd.DataFrame([transitions[k] for k in index], index=index)
         return df.fillna(method='ffill')
 
-    def get_expected_power_df(self):
+    def _get_task_cpu_df(self):
+        """
+        Get a DataFrame mapping task names to the CPU they ran on
+
+        Use the sched_switch trace event to find which CPU each task ran
+        on. Does not reflect idleness - tasks not running are shown as running
+        on the last CPU they woke on.
+
+        :returns: A Pandas DataFrame with a column for each task, showing the
+                  CPU that the task was "on" at each moment in time
+        """
+        tasks = self.rtapp_params.keys()
+        trace = self.trace
+
+        df = trace.ftrace.sched_switch.data_frame[['next_comm', '__cpu']]
+        df = df[df['next_comm'].isin(tasks)]
+        df = df.pivot(index=df.index, columns='next_comm').fillna(method='ffill')
+        cpu_df = df['__cpu']
+        # Drop consecutive duplicates
+        cpu_df = cpu_df[(cpu_df.shift(+1) != cpu_df).any(axis=1)]
+        return cpu_df
+
+    def _sort_power_df_columns(self, df):
+        """
+        Helper method to re-order the columns of a power DataFrame
+
+        This has no significance for code, but when examining DataFrames by hand
+        they are easier to understand if the columns are in a logical order.
+        """
+        node_cpus = [node.cpus for node in self.nrg_model.root.iter_nodes()]
+        return pd.DataFrame(df, columns=[c for c in node_cpus if c in df])
+
+    def _get_expected_power_df(self):
         """
         Estimate *optimal* power usage over time
 
@@ -291,15 +412,15 @@ class GenericTestBundle(TestBundle):
                   "power" column with the sum of other columns. Shows the
                   estimated *optimal* power over time.
         """
-        task_utils_df = self.get_task_utils_df()
+        task_utils_df = self._get_expected_task_utils_df()
 
         data = []
         index = []
 
         def exp_power(row):
             task_utils = row.to_dict()
-            expected_utils = nrg_model.get_optimal_placements(task_utils)[0]
-            power = nrg_model.estimate_from_cpu_util(expected_utils)
+            expected_utils = self.nrg_model.get_optimal_placements(task_utils)[0]
+            power = self.nrg_model.estimate_from_cpu_util(expected_utils)
             columns = power.keys()
 
             # Assemble a dataframe to plot the expected utilization
@@ -311,9 +432,51 @@ class GenericTestBundle(TestBundle):
         res_df = self._sort_power_df_columns(
             task_utils_df.apply(exp_power, axis=1))
 
-        self.plot_expected_util(experiment, pd.DataFrame(data, index=index))
+        #self._plot_expected_util(pd.DataFrame(data, index=index))
 
         return res_df
+
+    def _get_estimated_power_df(self):
+        """
+        Considering only the task placement, estimate power usage over time
+
+        Examine a trace and use :meth:EnergyModel.estimate_from_cpu_util to get
+        a DataFrame showing the estimated power usage over time. This assumes
+        perfect cpuidle and cpufreq behaviour.
+
+        :returns: A Pandas DataFrame with a column node in the energy model
+                  (keyed with a tuple of the CPUs contained by that node) Shows
+                  the estimated power over time.
+        """
+        task_cpu_df = self._get_task_cpu_df()
+        task_utils_df = self._get_expected_task_utils_df()
+        task_utils_df.index = [time + self._get_start_time() for time in task_utils_df.index]
+        tasks = self.rtapp_params.keys()
+
+        # Create a combined DataFrame with the utilization of a task and the CPU
+        # it was running on at each moment. Looks like:
+        #                       utils                  cpus
+        #          task_wmig0 task_wmig1 task_wmig0 task_wmig1
+        # 2.375056      102.4      102.4        NaN        NaN
+        # 2.375105      102.4      102.4        2.0        NaN
+
+        df = pd.concat([task_utils_df, task_cpu_df],
+                       axis=1, keys=['utils', 'cpus'])
+        df = df.sort_index().fillna(method='ffill')
+
+        # Now make a DataFrame with the estimated power at each moment.
+        def est_power(row):
+            cpu_utils = [0 for cpu in self.nrg_model.cpus]
+            for task in tasks:
+                cpu = row['cpus'][task]
+                util = row['utils'][task]
+                if not isnan(cpu):
+                    cpu_utils[int(cpu)] += util
+            power = self.nrg_model.estimate_from_cpu_util(cpu_utils)
+            columns = power.keys()
+            return pd.Series([power[c] for c in columns], index=columns)
+        return self._sort_power_df_columns(df.apply(est_power, axis=1))
+
 
     def test_task_placement(self, energy_est_threshold_pct=5):
         """
@@ -323,13 +486,13 @@ class GenericTestBundle(TestBundle):
             optimal task placement energy cost
         :type energy_est_threshold_pct: int
 
-        Use :meth:`get_expected_power_df` and :meth:`get_power_df` to estimate
-        optimal and observed power usage for task placements of the experiment's
-        workload. Assert that the observed power does not exceed the optimal
-        power by more than :attr:energy_est_threshold_pct percents.
+        Compute optimal energy consumption (energy-optimal task placement)
+        and compare to energy consumption estimated from the trace.
+        Check that the estimated energy does not exceed the optimal energy by
+        more than :attr:`energy_est_threshold_pct` percents.
         """
-        exp_power = self.get_expected_power_df(experiment)
-        est_power = self.get_power_df(experiment)
+        exp_power = self._get_expected_power_df()
+        est_power = self._get_estimated_power_df()
 
         exp_energy = area_under_curve(exp_power.sum(axis=1), method='rect')
         est_energy = area_under_curve(est_power.sum(axis=1), method='rect')
@@ -337,10 +500,19 @@ class GenericTestBundle(TestBundle):
         msg = 'Estimated {} bogo-Joules to run workload, expected {}'.format(
             est_energy, exp_energy)
         threshold = exp_energy * (1 + (energy_est_threshold_pct / 100.))
-        self.assertLess(est_energy, threshold, msg=msg)
 
-from wlgen.rta import Periodic
+        passed = est_energy < threshold
+        res = ResultBundle(passed)
+        res.add_metric(Metric("estimated_energy", est_energy, units='bogo-joules',
+                              lower_is_better=True))
+        res.add_metric(Metric("energy_threshold", threshold, units='bogo-joules',
+                              lower_is_better=True))
+        return res
 
+
+from wlgen.rta import Periodic, Ramp, Step
+
+# TODO: factorize this crap out of these classes
 class OneSmallTask(GenericTestBundle):
     """
     A single 'small' (fits in a LITTLE) task
@@ -351,7 +523,7 @@ class OneSmallTask(GenericTestBundle):
     @classmethod
     def create_rtapp_params(cls, te):
         # 50% of the smallest CPU's capacity
-        duty = int((min(te.target.sched.get_capacities().values()) / 1024.) * 50)
+        duty = int((cls.min_cpu_capacity(te) / 1024.) * 50)
 
         rtapp_params = {}
         rtapp_params[cls.task_name] = Periodic(
@@ -359,6 +531,139 @@ class OneSmallTask(GenericTestBundle):
             duration_s=1,
             period_ms=16
         )
+
+        return rtapp_params
+
+class ThreeSmallTasks(GenericTestBundle):
+    """
+    Three 'small' (fits in a LITTLE) tasks
+    """
+    task_prefix = "small"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        # 50% of the smallest CPU's capacity
+        duty = int((cls.min_cpu_capacity(te) / 1024.) * 50)
+
+        rtapp_params = {}
+        for i in range(3):
+            rtapp_params["{}_{}".format(cls.task_prefix, i)] = Periodic(
+                duty_cycle_pct=duty,
+                duration_s=1,
+                period_ms=16
+            )
+
+        return rtapp_params
+
+class TwoBigTasks(GenericTestBundle):
+    """
+    """
+
+    task_prefix = "big"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        # 80% of the biggest CPU's capacity
+        duty = int((cls.max_cpu_capacity(te) / 1024.) * 80)
+
+        rtapp_params = {}
+        for i in range(2):
+            rtapp_params["{}_{}".format(cls.task_prefix, i)] = Periodic(
+                duty_cycle_pct=duty,
+                duration_s=1,
+                period_ms=16
+            )
+
+        return rtapp_params
+
+class TwoBigThreeSmall(GenericTestBundle):
+    """
+    """
+
+    small_prefix = "small"
+    big_prefix = "big"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        # 50% of the smallest CPU's capacity
+        small_duty = int((cls.min_cpu_capacity(te) / 1024.) * 50)
+        # 80% of the biggest CPU's capacity
+        big_duty = int((cls.max_cpu_capacity(te) / 1024.) * 80)
+
+        rtapp_params = {}
+
+        for i in range(3):
+            rtapp_params["{}_{}".format(cls.small_prefix, i)] = Periodic(
+                duty_cycle_pct=small_duty,
+                duration_s=1,
+                period_ms=16
+            )
+
+        for i in range(2):
+            rtapp_params["{}_{}".format(cls.big_prefix, i)] = Periodic(
+                duty_cycle_pct=big_duty,
+                duration_s=1,
+                period_ms=16
+            )
+
+        return rtapp_params
+
+class RampUp(GenericTestBundle):
+    """
+    """
+    task_name = "ramp_up"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        rtapp_params = {}
+        rtapp_params[cls.task_name] = Ramp(
+            start_pct=5,
+            end_pct=70,
+            delta_pct=5,
+            time_s=.5,
+            period_ms=16
+        )
+
+        return rtapp_params
+
+class RampDown(GenericTestBundle):
+    """
+    """
+    task_name = "ramp_down"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        rtapp_params = {}
+        rtapp_params[cls.task_name] = Ramp(
+            start_pct=70,
+            end_pct=5,
+            delta_pct=5,
+            time_s=.5,
+            period_ms=16
+        )
+
+        return rtapp_params
+
+class EnergyModelWakeMigration(GenericTestBundle):
+    """
+    """
+    task_prefix = "emwm"
+
+    @classmethod
+    def create_rtapp_params(cls, te):
+        rtapp_params = {}
+        capacities = te.target.sched.get_capacities()
+        max_capa = cls.max_cpu_capacity(te)
+        bigs = [cpu for cpu, capacity in capacities.items() if capacity == max_capa]
+
+        for i in range(len(bigs)):
+            rtapp_params["{}_{}".format(cls.task_prefix, i)] = Step(
+                start_pct=10,
+                end_pct=70,
+                time_s=2,
+                loops=2,
+                period_ms=16
+            )
 
         return rtapp_params
 
@@ -375,8 +680,6 @@ from devlib.module.hotplug import HotplugModule
 from devlib.exception import TimeoutError
 
 class HotplugTestBundle(TestBundle):
-
-    # /!\ TODO /!\ target alive AND hotplug state coherent
 
     def __init__(self, target_alive, hotpluggable_cpus, live_cpus):
         self.target_alive = target_alive
