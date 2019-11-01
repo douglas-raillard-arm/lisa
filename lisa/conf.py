e773b65a8173c71ef827f7d6c8350cea3dcd4f6a
douglas-raillard-arm <public.douglas.raillard@gmail.com>
2019-11-01 19:45:08 +0000

    lisa.platforms.platinfo: Add kernel symbol addresses
    Parse the content of /proc/kallsyms in order to be able to resolve
    kernel addresses to symbol name.

--- lisa/conf.py
+++ lisa/conf.py
@@ -1613,4 +1613,7 @@ class StrList(TypedList):
 class StrIntListDict(TypedDict):
     _type = (str, IntList)
 
+class IntStrDict(TypedDict):
+    _type = (int, str)
+
 # vim :set tabstop=4 shiftwidth=4 textwidth=80 expandtab
